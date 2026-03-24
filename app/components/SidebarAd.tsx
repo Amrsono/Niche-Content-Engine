dbf67ec9d1515ae3a7e7b105b3304881d19aaf46
Amrsono <amrsono@users.noreply.github.com>
2026-03-24 13:54:05 +0200

    style: improve sidebar ad placeholder visibility

--- app/components/SidebarAd.tsx
+++ app/components/SidebarAd.tsx
@@ -12,10 +12,13 @@ export default function SidebarAd() {
           <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
         </svg>
         
-        <p>Amazon Affiliate Skyscraper</p>
-        <span style={{ fontSize: '0.7rem', opacity: 0.5, marginTop: '5px' }}>
-          Code Placeholder
+        <p style={{ margin: '0 0 10px 0', fontWeight: 600 }}>Skyscraper Ad</p>
+        <span style={{ fontSize: '0.7rem', opacity: 0.6 }}>
+          Placeholder for<br/>Amazon Affiliate Link
         </span>
+        <div style={{ marginTop: '20px', fontSize: '0.6rem', color: '#3b82f6', fontWeight: 700 }}>
+          READY FOR CODE
+        </div>
       </div>
     </div>
   );
